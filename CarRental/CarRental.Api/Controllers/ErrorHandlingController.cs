c6595805220eebae8761fcd7e956089793e8c23b
Aymen Abdelmalik <167820077+MalikBNB@users.noreply.github.com>
2025-02-11 00:17:36 +0100

    Add Global Error Handling Middleware

---
+++ CarRental/CarRental.Api/Controllers/ErrorHandlingController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CarRental.Api.Controllers
+{
+    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ErrorHandlingController (ILogger<ErrorHandlingController> _logger) 
+        : ControllerBase
+    {
+
+        [Route("/error-development")]
+        public IActionResult HandleErrorDevelopment(
+                [FromServices] IHostEnvironment hostEnvironment)
+        {
+            if(!hostEnvironment.IsDevelopment())
+                return NotFound();
+
+            var exceptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>()!;
+
+            _logger.LogError(exceptionHandlerFeature.Error, exceptionHandlerFeature.Error.Message);
+
+            return Problem(
+                    detail: exceptionHandlerFeature.Error.StackTrace,
+                    title: exceptionHandlerFeature.Error.Message
+                );
+        }
+
+        [Route("/error")]
+        public IActionResult HandleError()
+        {
+            var exceptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>()!;
+
+            _logger.LogError(exceptionHandlerFeature.Error, exceptionHandlerFeature.Error.Message);
+
+            return Problem();
+        }
+    }
+}
